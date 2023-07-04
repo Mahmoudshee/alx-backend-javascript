7414f7d6a23f382153ff3146e69ac8f2dcff83ce
Mahmoudshee8914 <mahmoudshee8914@gmail.com>
2023-07-04 07:02:19 +0000

    kenty

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,7 @@
+function cleanSet(set, startString) {
+  const values = Array.from(set).filter((value) => value.startsWith(startString));
+  return values.map((value) => value.substring(startString.length)).join("-");
+}
+
+export default cleanSet;
+
